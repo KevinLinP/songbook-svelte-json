19a46e354260518c55d14774127170f075382ab8
Kevin Lin <kevin.lin.p@gmail.com>
2020-07-04 20:00:08 -0400

    porting from rollup

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { routes } from 'svelte-hash-router'
+import App from './App.svelte';
+import SongsPage from './SongsPage.svelte'
+routes.set({
+	'/': SongsPage,
+	'/:slug': SongsPage,
+})
+
+const app = new App({
+	target: document.body,
+});
+
+export default app;
